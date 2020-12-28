a6f54fee4dd9cbe034404b36a78e5b894e43dbb5
Anastasia Shimko <anastasiaishimko@gmail.com>
2020-12-28 22:51:39 +0300

    Add project files.

---
+++ HomeWorkLayots/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HomeWorkLayots.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
